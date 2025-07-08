e0248acc66334af3584cf4c4b9568184e817b180
lucasmoutinho2503 <moutinho319@gmail.com>
2025-07-08 10:35:04 -0300

    Primeiro commit

---
+++ src/test/java/one/api/forumhub/forumhub/ForumhubApplicationTests.java
@@ -0,0 +1,13 @@
+package one.api.forumhub.forumhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
